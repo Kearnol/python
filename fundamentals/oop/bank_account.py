e938bca81e8320b00593fe529f0015b4bd740319
Devin Kearns <devin.kearns@outlook.com>
2021-09-28 19:55:03 -0400

    Sensei challenge + transfer money

--- fundamentals/oop/bank_account.py
+++ fundamentals/oop/bank_account.py
@@ -1,3 +1,46 @@
+class User:
+    all_accounts = []
+    def __init__(self, first_name, last_name, email_address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email_addres = email_address
+        self.accounts = {
+            "checking": BankAccount(0.02, 0),
+            "saving": BankAccount(0.02, 0),
+            "investment": BankAccount(0.02, 0)
+        }
+        User.all_accounts.append(self)
+
+    def make_deposit(self, amount, bankID="checking"):
+        self.accounts[bankID].deposit(amount)
+        return self
+
+    def make_withdrawal(self, amount, bankID="checking"):
+        self.accounts[bankID].withdraw(amount)
+        return self
+        
+    def display_user_balance(self, bankID="checking"):
+        print(f"User {self.first_name} {self.last_name}'s Balance is: ${self.accounts[bankID].account_balance}")
+        return self
+    
+    def transfer_money(self, other_user, amount, bankIDfrom="checking", bankIDto="saving"):
+        self.make_withdrawal(amount, bankIDfrom)
+        other_user.make_deposit(amount, bankIDto)
+        print("After transfer:")
+        self.display_user_balance()
+        other_user.display_user_balance()
+        print("Transfer end.")
+        return self
+
+    @classmethod
+    def print_all_account_data(cls):
+        for x in cls.all_accounts:
+            print(f"{x.first_name} {x.last_name} | Account Balances:")
+            for key, value in x.accounts.items():
+                print(key, value.account_balance)
+
+    
+
 class BankAccount:
     all_accounts = []
 
@@ -8,7 +51,6 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"After deposit, new account balance = ${self.account_balance}")
         return self
 
     def withdraw(self, amount):
@@ -18,7 +60,6 @@ class BankAccount:
             print(f"Account balance = ${self.account_balance}")
         else:
             self.account_balance -= amount
-            print(f"After withdraw, account balance = ${self.account_balance}")
         return self
     
     def display_account_info(self):
@@ -35,12 +76,19 @@ class BankAccount:
         for accounts in cls.all_accounts:
             print(f"Balance: ${accounts.account_balance}, Interest: {accounts.int_rate}")
 
+#make Users:
+devin = User("Devin","Kearns","dkearn@fake.com")
+troy = User("Troy", "Mild", "TMild@fake.com")
+bob = User("Bob", "Lanchuck", "BobyFlay@yahooper.com")
 
-account1 = BankAccount(0.05, 100)
-account2 = BankAccount(0.03, 100)
-BankAccount.print_all_accounts()
-print("")
-account1.deposit(200).deposit(100).deposit(50).yeild_interest().display_account_info().withdraw(475)
-account2.deposit(300).deposit(200).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yeild_interest().display_account_info()
-print("")
+print(f"Account = {bob.accounts}")
 
+# bob.make_deposit(200, "checking").display_user_balance()
+
+#Process Transactions
+devin.make_deposit(200).make_deposit(200).make_deposit(200).make_withdrawal(300).display_user_balance().transfer_money(troy,200, "checking", "investment")
+troy.make_deposit(100).make_deposit(100).make_withdrawal(50).make_withdrawal(75).display_user_balance()
+bob.make_deposit(500).make_withdrawal(175).make_withdrawal(100).make_withdrawal(25).display_user_balance()
+
+#Print Account details
+User.print_all_account_data()
