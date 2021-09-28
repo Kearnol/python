0a9167ed244c8eb1320e043bce79f33ff8794769
Devin Kearns <devin.kearns@outlook.com>
2021-09-28 14:28:30 -0400

    updated methods to chain

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -7,12 +7,15 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
         
     def display_user_balance(self):
         print(f"User {self.first_name} {self.last_name}'s Balance is: ${self.account_balance}")
+        return self
     
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
@@ -20,25 +23,15 @@ class User:
         print("After transfer:")
         self.display_user_balance()
         other_user.display_user_balance()
+        print("Transfer end.")
+        return self
 
 devin = User("Devin","Kearns","dkearn@fake.com")
 troy = User("Troy", "Mild", "TMild@fake.com")
 bob = User("Bob", "Lanchuck", "BobyFlay@yahooper.com")
 
-devin.make_deposit(200)
-devin.make_deposit(200)
-devin.make_deposit(200)
-devin.make_withdrawal(300)
-devin.display_user_balance()
-troy.make_deposit(100)
-troy.make_deposit(100)
-troy.make_withdrawal(50)
-troy.make_withdrawal(75)
-troy.display_user_balance()
-bob.make_deposit(500)
-bob.make_withdrawal(175)
-bob.make_withdrawal(100)
-bob.make_withdrawal(25)
-bob.display_user_balance()
-devin.transfer_money(bob, 100)
+devin.make_deposit(200).make_deposit(200).make_deposit(200).make_withdrawal(300).display_user_balance().transfer_money(bob, 100)
+troy.make_deposit(100).make_deposit(100).make_withdrawal(50).make_withdrawal(75).display_user_balance()
+bob.make_deposit(500).make_withdrawal(175).make_withdrawal(100).make_withdrawal(25).display_user_balance()
+
 
