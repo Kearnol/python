b3a4dbbbf58561c9ccce0c26b33c2b6dadc6f18b
Devin Kearns <devin.kearns@outlook.com>
2021-10-01 00:58:41 -0400

    initial commit

--- flask/checkerboard/server.py
+++ flask/checkerboard/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask.templating import render_template_string
 
 app = Flask(__name__)
 
@@ -14,9 +15,9 @@ def display_checkerboard_height(height):
 def display_checkerboard_num(width, height):
     return render_template("index.html", height=height, width=width)
 
-@app.route("/10/10")
-def display_checkerboard_10():
-    return render_template("index.html", height=10, width=10)
+@app.route("/<int:width>/<int:height>/<string:color1>/<string:color2>")
+def display_checkerboard_num_color(width, height, color1, color2):
+    return render_template("index.html", height=height, width=width, color1=color1, color2=color2) 
 
 if __name__ == "__main__":
     app.run(debug=True)
